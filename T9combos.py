d248f5d77bdfb82f23f69c230bc28c75fc0581d1
Henry Li <henry.s.leigh@gmail.com>
2017-11-25 14:45:07 -0800

    added phone number letter combinations

---
+++ T9combos.py
@@ -0,0 +1,49 @@
+# T9combos.py
+
+def charFromNumber(digit):
+	if digit == 1:
+		return ["*"]
+	if digit == 0:
+		return [" "]
+	if 1 < digit < 7:
+		start = 97
+		offset = (digit-2)*3
+		i = start + offset
+		return [chr(i),chr(i+1),chr(i+2)]
+	if digit == 7:
+		return ["p","q","r","s"]
+	if digit == 8:
+		return ["t","u","v"]
+	if digit == 9:
+		return ["w","x","y","z"]
+
+def letterCombinations(digits):
+	ds = digits[::-1]
+	# print(ds)
+	ans = None
+	for d in ds:
+		if not ans:
+			ans = charFromNumber(int(d))
+		else:
+			tmp = []
+			for ch in charFromNumber(int(d)):
+				# print(ch)
+				for sub in ans:
+					# tmp.append(ch)
+					tmp.append(ch+sub)
+					# print(tmp)
+			ans = tmp
+		# print(charFromNumber(int(d)))
+		# print(ans)
+	return ans
+
+# print(charFromNumber(2))
+# print(charFromNumber(5))
+# print(charFromNumber(1))
+# print(charFromNumber(0))
+# print(charFromNumber(7))
+# print(charFromNumber(8))
+# print(charFromNumber(9))
+
+
+print(letterCombinations("23"))
