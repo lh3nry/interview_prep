b294982b5ac7bff39de5f094008118c928fc299d
Henry Li <henry.s.leigh@gmail.com>
2017-12-01 18:43:49 -0800

    special division completion

--- sp_division.py
+++ sp_division.py
@@ -1,35 +1,21 @@
 # sp_division.py
 def divide(dividend,divisor):
-	p,c = 0,divisor
 	power = 1
-
 	mul = divisor
+
 	while (mul << 1) < dividend:
-		# p = c
 		mul <<= 1
 		power<<=1
 
-	# count2 = pow(2,power)
 	ans = power
-	print(p,c)
+
 	residual = dividend - mul
-	print(power,residual)
+	# print(power,residual)
 	while residual >= divisor:
 		residual-=divisor
 		ans+=1
-	print(residual)
+	# print(residual)
 	return ans
-
-	# result = 0
-	# while dividend>=divisor:
-	# 	s,power = divisor,1
-	# 	while (s<<1) <= dividend:
-	# 		s<<=1
-	# 		power<<=1
-	# 	result+=power
-	# 	dividend-=s
-
-	# return result
 
 a = 21
 b = 7
