d6e94d96f1cf2f2b622d268dd7298bec102c54cc
Henry Li <henry.s.leigh@gmail.com>
2017-12-01 19:48:40 -0800

    validate sudoku accepted

--- valid_sudoku.py
+++ valid_sudoku.py
@@ -2,19 +2,33 @@
 import pprint as pp
 def checksquare(o,board):
 	seen = {}
-	a,b = o[0], o[1]
+	a,b = 3*o[0], 3*o[1]
+	sq = []
 	for i in range(3):
 		for j in range(3):
 			x = board[a+i][b+j]
 			# print((a+i,b+j),x)
-			if x != '.' and x in seen:
-				return False
-			seen[x] = 1
+			sq.append(x)
+			# if x != '.' and x in seen:
+			# 	return False
+			# seen[x] = 1
+
+	# for i in range(3):
+	# 	# for j in range(3):
+	# 		print(sq[i*3+0],sq[i*3+1],sq[i*3+2])
+
+	for x in sq:
+		if x != '.' and x in seen:
+			return False
+		seen[x] = 1
+	# print("sq pass")
+	# print(" ")
+
 	return True
 
 
 def validate(board):
-	# pp.pprint(board)
+	pp.pprint(board)
 	for row in board:
 		seen = {}
 		for x in row:
@@ -22,18 +36,25 @@ def validate(board):
 			if x != '.' and x in seen:
 				return False
 			seen[x] = 1
+	print("rows passed")
+
 
 	for col in range(9):
 		seen = {}
 		for i in range(9):
+			x = board[i][col]
 			if x != '.' and x in seen:
 				return False
 			seen[x] = 1
+	print("cols passed")
 
 	for r in range(3):
 		for c in range(3):
 			if not checksquare((r,c),board):
 				return False
+	print("squares passed")
+
+
 	return True
 
 
@@ -50,5 +71,21 @@ board = [
 ["8",".",".",".",".",".",".",".","."],
 ["9",".",".",".",".",".",".",".","."]
 ]
+
+board2 = [[".",".","4",".",".",".","6","3","."],
+[".",".",".",".",".",".",".",".","."],
+["5",".",".",".",".",".",".","9","."],
+[".",".",".","5","6",".",".",".","."],
+["4",".","3",".",".",".",".",".","1"],
+[".",".",".","7",".",".",".",".","."],
+[".",".",".","5",".",".",".",".","."],
+[".",".",".",".",".",".",".",".","."],
+[".",".",".",".",".",".",".",".","."]]
+
+board3 = [[".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".","3",".","."],[".",".",".","1","8",".",".",".","."],[".",".",".","7",".",".",".",".","."],[".",".",".",".","1",".","9","7","."],[".",".",".",".",".",".",".",".","."],[".",".",".","3","6",".","1",".","."],[".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".","2","."]]
+
 # print(checksquare((0,3),board))
-print(validate(board))
+print(validate(board3))
+
+
+
