90dd3475c7996d4b589993e9fb7063c608df8f87
Henry Li <henry.s.leigh@gmail.com>
2017-12-01 19:14:02 -0800

    special division TLE

--- sp_division.py
+++ sp_division.py
@@ -1,30 +1,34 @@
 # sp_division.py
 def divide(dividend,divisor):
-	if divisor == 0: return -1
-	if dividend == 0: return 0
+    if divisor == 0: return -1
+    if dividend == 0: return 0
 
-	sign = not ((divisor < 0) ^ (dividend < 0))
-	dividend = abs(dividend)
-	divisor = abs(divisor)
+    sign = not ((divisor < 0) ^ (dividend < 0))
+    dividend = abs(dividend)
+    divisor = abs(divisor)
+    if divisor > dividend: return 0
+    if divisor == 1: return dividend if sign else -dividend
 
-	power = 1
-	mul = divisor
+    power = 1
+    mul = divisor
 
-	while (mul << 1) < dividend:
-		mul <<= 1
-		power<<=1
+    while (mul << 1) < dividend:
+        mul <<= 1
+        power<<=1
 
-	ans = power
+    ans = power
 
-	residual = dividend - mul
-	# print(power,residual)
-	while residual >= divisor:
-		residual-=divisor
-		ans+=1
-	# print(residual)
-	return ans if sign else -ans
+    residual = dividend - mul
+    # print(power,residual)
+    while residual >= divisor:
+        residual-=divisor
+        ans+=1
+    # print(residual)
+    return ans if sign else -ans
 
-a = -21
-b = 7
+a = -2147483648
+b = -1
+
+
 
 print(divide(a,b),a//b)
