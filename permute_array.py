bd2764024a77cc0f56ecdfb2b78bb689d613fe5a
Henry Li <henry.s.leigh@gmail.com>
2017-12-03 01:57:37 -0800

    permutations - two solutions; had to instantiate new temp lists in order to prevent repeated or erronenous output

--- permute_array.py
+++ permute_array.py
@@ -4,38 +4,52 @@ def permute_w(nums,permuted,acc):
 	# print(nums)
 	if len(permuted) == len(nums):
 		# if permuted not in acc: 
-		acc.append(permuted)
-		print(acc,id(acc))
-		print(permuted,id(permuted))
+		new = list(permuted)
+		acc.append(new)
+		# print(acc,id(acc))
+		# print(permuted,id(permuted))
 		return
 	else:
 		for x in range(len(nums)):
 			# print(x)
 			if nums[x] in permuted: continue
 			permuted.append(nums[x])
-			# left = nums[:x] + nums[x+1:]
-			# print(left)
 			permute_w(nums,permuted,acc)
-			# print(
 			permuted.pop()
-			# )
-
-  
 
 
 
 def permute(nums):
-	# print(nums)
 	a,tmp,copy = [],[], nums
 	permute_w(copy,tmp,a)
-	print("")
 	return a
 
 
-arr = [1,2,3]
+
+def backtrack(pos,num,acc):
+	if pos == len(num):
+		# print(num)
+		# construct new lists (gives new id so the recursion will work)
+		new = list(num)
+		# print(id(new),id(num))
+		acc.append(new)
+	else:
+		for i in range(pos,len(num)):
+			num[i],num[pos] = num[pos],num[i]
+			backtrack(pos+1,num,acc)
+			num[i],num[pos] = num[pos],num[i]
+
+
+
+arr = [1,2,3,4]
 
 # print(permute([]))
 
 a = permute(arr)
 print(a,id(a))
+print("")
+
+a = []
+backtrack(0,arr,a)
+print(a)
 
