7c0de2693799d0bdc78b5f08830ee10d1264c4a2
Henry Li <henry.s.leigh@gmail.com>
2018-01-02 07:54:36 -0800

    trie pass with tests

--- trie.py
+++ trie.py
@@ -8,19 +8,21 @@ class TrieNode:
         
     def insert(self,wd):
         node = self
+        # update the root node counter 
+        # (each insert adds a new word)
         node.count+=1
+
         for ch in wd:
             if ch not in node.next:
                 new = TrieNode()
                 new.data = ch
-                # node.count += 1
                 new.count = 1
                 node.next[ch] = new
                 node = node.next[ch]
             else:
                 node = node.next[ch]
                 node.count += 1
-        # node.count+=1
+
     def find(self, wd):
         node = self
         for ch in wd:
@@ -36,5 +38,8 @@ tn = TrieNode()
 
 tn.insert(test)
 tn.insert("hack")
-print(tn.count)
-print(tn.find("hackerra"))
+# print(tn.count)
+# print(tn.find("hackerra"))
+print(tn.find("hac"))
+print(tn.find("hak"))
+print(tn.find("hacker"))
