94e2c0c805891c5722a113f7667906726caa41bc
Henry Li <henry.s.leigh@gmail.com>
2018-01-15 20:49:43 -0800

    coins accepted, added a few optimizations

--- coins.py
+++ coins.py
@@ -2,16 +2,18 @@
 def coin_bag(n,coins):
 	arr = [0]*(n+1)
 	for c in coins:
-		print("c =",c)
-		if c <= n: arr[c] += 1
-		for m in range(1,n+1):
-			print("\tm =",m)
-			if m-c >= 0:
-				print("\t\tlookback",m-c,arr[m-c])
-				arr[m] = arr[m] + arr[m-c]
+		# print("c =",c)
+		if c <= n: arr[c] += 1	# can we use coin c at all?
+
+		for m in range(c,n+1):
+			# Start at m = c to eliminate (>= 0) branch
+			# print("\tm =",m)
+			# print("\t\tlookback",m-c,arr[m-c])
+			arr[m] = arr[m] + arr[m-c]
 
 	print(arr)
+	return arr[n]
 
 
-coin_bag(4,[1,2,3])
-coin_bag(10,[2,5,3,6])
+print(coin_bag(4,[1,2,3]))
+print(coin_bag(10,[2,5,3,6]))
