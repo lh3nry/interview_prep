579df9f2443dffd6c453e1d5d075074f123fc6f9
Henry Li <henry.s.leigh@gmail.com>
2018-01-20 19:45:50 -0800

    reorganizeString second try

--- contest68.py
+++ contest68.py
@@ -23,16 +23,15 @@ def reorganizeString(S):
     pos,ind = 1,0
 
     while hist_s:
-    	ind=(ind+1)%len(hist_s)
-    	# print(len(hist_s),ind)
-    	char,count = hist_s[ind][0],hist_s[ind][1]
-    	if count == 0: 
-    		hist_s.remove((char,count))
-    		continue
-    	new = new[:pos] + [char] + new[pos:]
-    	pos+=2
-    	hist_s[ind]=(hist_s[ind][0],hist_s[ind][1]-1)
-    	# print(new,hist_s)
+        ind=(ind+1)%len(hist_s)
+        # print(len(hist_s),ind)
+        char,count = hist_s[ind][0],hist_s[ind][1]
+        for i in range(count):
+            new = new[:pos] + [char] + new[pos:]
+            pos+=2
+        hist_s.remove((char,count))
+        pos = 1
+        # print(new,hist_s)
     return "".join(new)
 
 
